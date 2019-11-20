fe0283fbe26b1fa1728d784c626d631b382e7e0e
Luca Acchione <lucaacchione@Lucas-Air.ACCHIONE>
2019-11-20 15:41:03 -0500

    Changed up the Lightbox effect. Also added a new feature to include a subtitle to the lightbox.

--- source/js/main.ts
+++ source/js/main.ts
@@ -1,6 +1,8 @@
 let imgLightbox = <HTMLElement>document.body.querySelector("img");
+let lightboxFont = <HTMLElement>document.body.querySelector(".hidden");
 
 imgLightbox.addEventListener("click", function() {
 
     imgLightbox.classList.toggle("lightbox");
+    lightboxFont.classList.toggle("show");
 });
